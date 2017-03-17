0e79b595b131d822ef8a6b360cea4f49876a5e50
Brynna <brynna.mccarter@gmail.com>
2017-03-17 11:54:14 -0700

    add readme

---
+++ README.md
@@ -0,0 +1,25 @@
+## Install
+
+```
+$ npm install
+```
+
+```
+$ bower install
+```
+
+## API
+
+## Built with Pageres
+
+- [Break Shot](https://github.com/victorferraz/break-shot) - Desktop app for capturing screenshots of responsive websites.
+
+
+## Team
+
+[![Brynna Klamkin-McCarter Sorhus](https://www.linkedin.com/in/brynna-mccarter-9a753343/)
+---|---|---
+
+## License
+
+MIT © [Brynna Klamkin-McCarter]
--- build/js/app.js
+++ build/js/app.js
@@ -7,7 +7,7 @@ Doctor.prototype.find = function(disease) {
     $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + disease + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=4e8845222bc6a858d55f09ca29dc833a')
      .then(function(result) {
         for (var i = 0; i < result.data.length; i++) {
-          $('.showDoctors').append('<table class="table">' + '<thead>' + '<tr>' + '<th> Name </th>' + '<th> Bio </th>' + '</tr>' + '</thead>' + '<tbody>' + '<tr>' + '<td>' + result.data[i].profile.last_name + ', ' + result.data[i].profile.first_name + ', ' +  result.data[i].profile.title + '</td>' + '<td>' +  result.data[i].profile.bio + '</td>' +  '</tr>' + '</tbody>' + '</table>');
+          $('.showDoctors').append('<tr>' + '<th scope="row">' + [i + 1] + '</th>' + '<td>' + result.data[i].profile.first_name + ' ' + '</td>' + '<td>' + result.data[i].profile.last_name + '</td>' + '<td>' +  result.data[i].profile.bio + '</td>' +  '</tr>');
           console.log(result.data[i].profile.first_name);
         }
       })
