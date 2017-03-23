2f815f63e1e02d99761e29d94c8354eb6c8be871
Sean Peterson <seanpeterson11@gmail.com>
2017-03-23 16:12:39 -0700

    add callback

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -5,7 +5,12 @@ $(document).ready(function() {
     var disease = $('#disease').val();
     $('#disease').val("");
     var newDoctor = new Doctor(disease);
-    var result = newDoctor.find(disease);
+    var result = newDoctor.find(disease, function(result) {
+      $('.showDoctors').text("");
+      for (var i = 0; i < result.data.length; i++) {
+        $('.showDoctors').append('<tr>' + '<th scope="row">' + [i + 1] + '</th>' + '<td>' + result.data[i].profile.first_name + ' ' + '</td>' + '<td>' + result.data[i].profile.last_name + '</td>' + '<td>' +  result.data[i].profile.bio + '</td>' +  '</tr>');
+      }
+     });
     $('.resultsBanner').text(' Your results for ' + disease + ' are:');
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -4,14 +4,10 @@ function Doctor(disease) {
   this.disease = disease;
 }
 
-Doctor.prototype.find = function(disease) {
+Doctor.prototype.find = function(disease, callback) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + disease + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
-     $('.showDoctors').text("");
-      for (var i = 0; i < result.data.length; i++) {
-        $('.showDoctors').append('<tr>' + '<th scope="row">' + [i + 1] + '</th>' + '<td>' + result.data[i].profile.first_name + ' ' + '</td>' + '<td>' + result.data[i].profile.last_name + '</td>' + '<td>' +  result.data[i].profile.bio + '</td>' +  '</tr>');
-        console.log(result.data[i].profile.first_name);
-      }
+    callback(result);
     })
    .fail(function(error){
       // console.log("fail");
