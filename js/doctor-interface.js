3dea49eae384ae9d41684c3086daab22233152e4
Brynna <brynna.mccarter@gmail.com>
2017-04-13 06:49:17 -0700

    remove gulp files to prepare for hosting

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,4 +1,18 @@
-var Doctor = require('./../js/doctor.js').doctorModule;
+var apiKey = "4e8845222bc6a858d55f09ca29dc833a";
+
+function Doctor(disease) {
+  this.disease = disease;
+}
+
+Doctor.prototype.find = function(disease, callback) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + disease + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+   .then(function(result) {
+    callback(result);
+    })
+   .fail(function(error){
+      // console.log("fail");
+    });
+};
 
 $(document).ready(function() {
   $('#submit').click(function() {
